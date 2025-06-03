390b7336e499ea7eb9c0c6761f52136d5ef7ef8b
Vitor <vitor.lira06@aluno.ifce.edu.br>
2025-06-03 14:02:51 -0300

    teste

---
+++ provas/prova_pratica_02/bin/prova_pratica_02.dart
@@ -0,0 +1,5 @@
+import 'package:prova_pratica_02/prova_pratica_02.dart' as myFile;
+
+void main(List<String> arguments) {
+  myFile.run();
+}
---
+++ provas/prova_pratica_02/test/prova_pratica_02_test.dart
@@ -0,0 +1,6 @@
+import 'package:prova_pratica_02/prova_pratica_02.dart';
+import 'package:test/test.dart';
+
+void main() {
+
+}
