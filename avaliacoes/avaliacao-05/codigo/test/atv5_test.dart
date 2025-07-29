153a79bca7bc9573b7d012384869d811655b4903
Vitor <adrianovor@gmail.com>
2025-07-29 20:57:18 -0300

    push dos pdf's

--- avaliacoes/avaliacao-05/codigo/test/atv5_test.dart
+++ avaliacoes/avaliacao-05/codigo/test/atv5_test.dart
@@ -1,4 +1,3 @@
-import 'package:atv5/atv5.dart';
 import 'package:test/test.dart';
 
 void main() {
--- provas/prova_pratica_02/test/prova_pratica_02_test.dart
+++ provas/prova_pratica_02/test/prova_pratica_02_test.dart
@@ -1,6 +1,1 @@
-import 'package:prova_pratica_02/prova_pratica_02.dart';
-import 'package:test/test.dart';
-
-void main() {
-
-}
+void main() {}
